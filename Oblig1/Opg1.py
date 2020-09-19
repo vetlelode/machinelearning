232197dd087c7b0e93240d6ea89e4c5f63828483
Vetle Lode <vetlelode@gmail.com>
2020-09-19 23:29:40 +0200

    More progress

--- Oblig1/Opg1.py
+++ Oblig1/Opg1.py
@@ -2,23 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statistics
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 data_tot = pd.read_csv('ex1data1.csv')
+# Explore the shape of the data
+print(data_tot.shape)
 
 # first 80% of the dataset
 data = data_tot[:77]
 # last 20% of the dataset
 test = data_tot[-20:]
 
+
 X_test = test.iloc[:, 0]
 Y_test = test.iloc[:, 1]
 X = data.iloc[:, 0]
 Y = data.iloc[:, 1]
 
-m = 0
-c = 0
+m, c = 0
 n = float(len(X))
-
 L = 0.0001
 
 for i in range(2000):
--- Oblig1/Opg2.py
+++ Oblig1/Opg2.py
@@ -3,24 +3,43 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn import model_selection
+import matplotlib.pyplot as plt
 # This is functionally the same as reading the data from the CSV file in the assignment
 iris = datasets.load_iris()
 X_tot = iris.data
 y_tot = iris.target
-X_train, X_test, y_train, y_test = train_test_split(
-    X_tot, y_tot, test_size=0.25, random_state=42)
 
-model = KNeighborsClassifier(n_neighbors=15)
-result = model_selection.cross_val_score(model, X_test, y_test)
-est = model.fit(X_train, y_train)
-# run over the entirety of the test data and report the accuracy level
-predictions = est.predict(X_test)
-correct = 0
-for pred, actual in zip(predictions, y_test):
-    if pred == actual:
-        correct += 1
-print("The prediction Algorithm is:",
-      (correct/len(predictions))*100, "% correct (", correct, "/", len(predictions), ")")
+X_train, X_test, y_train, y_test = train_test_split(
+    X_tot, y_tot, test_size=0.25)
+
+
+accX = []
+accY = []
+# Test out 20 different variations of n_neighbours and find the most accurate one
+for i in range(20):
+    kfold = model_selection.KFold(n_splits=5, random_state="seed")
+    model = KNeighborsClassifier(n_neighbors=i+1).fit(X_train, y_train)
+    result = model_selection.cross_val_score(model, X_train, y_train, cv=kfold)
+    est = model
+    # run over the entirety of the test data and report the accuracy level
+    predictions = est.predict(X_test)
+    correct = 0
+    for pred, actual in zip(predictions, y_test):
+        if pred == actual:
+            correct += 1
+    print("The prediction Algorithm with n_neighbors = ", i, " is:",
+          (correct/len(predictions))*100, "% correct (", correct, "/", len(predictions), ")")
+    print(result.mean())
+
+    accY.append(result.mean())
+    accX.append(i+1)
+# Graph out the accuracy level of the varying levels of n_neighbours
+fig, ax = plt.subplots()
+ax.bar(accX, accY)
+ax.set_xlabel('n_neighbours')
+plt.xlim(0.5, 20.5)
+ax.set_ylabel('Mean accuracy')
+plt.show()
 
 #distances = list()
 #dst = float("+inf")
