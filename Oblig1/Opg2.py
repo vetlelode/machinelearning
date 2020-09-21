833c53479bec21f8d97614ea73affeefbf3b1433
Vetle Lode <vetlelode@gmail.com>
2020-09-21 17:20:55 +0200

    Donerino

--- Oblig1/Opg2.py
+++ Oblig1/Opg2.py
@@ -15,12 +15,16 @@ X_train, X_test, y_train, y_test = train_test_split(
 # These are used for the Graphing
 accX = []
 accY = []
+top_score = [420, 0.0]
 # Test out 20 different variations of n_neighbours and find the most accurate one
 for i in range(21):
-    kfold = model_selection.KFold(n_splits=5, random_state="seed")
+    kfold = model_selection.KFold(
+        n_splits=5, random_state="seed")
     model = KNeighborsClassifier(n_neighbors=i+1).fit(X_train, y_train)
     result = model_selection.cross_val_score(model, X_test, y_test, cv=kfold)
     print(i, result.mean())
+    if result.mean() > top_score[1]:
+        top_score = [i+1, result.mean()]
     accY.append(result.mean())
     accX.append(i+1)
 # Graph out the accuracy level of the varying levels of n_neighbours
@@ -31,4 +35,16 @@ ax.set_xlabel('n_neighbours')
 plt.xlim(0.5, 20.5)
 ax.set_ylabel('Mean accuracy')
 plt.show()
-plt.savefig("demo.png")
+
+# Use the top scoring n_neighbours as the goto
+kfold = model_selection.KFold(n_splits=5, random_state="seed")
+model = KNeighborsClassifier(n_neighbors=top_score[0]).fit(X_train, y_train)
+predictions = model.predict(X_test)
+correct = 0
+# run over the entirety of the test data and report the accuracy level
+# this is the equivulant of scoring the algorithm
+for pred, actual in zip(predictions, y_test):
+    if pred == actual:
+        correct += 1
+print("The prediction Algorithm with n_neighbors = ", top_score[0], " is:",
+      (correct/len(predictions))*100, "% correct (", correct, "/", len(predictions), ")")
