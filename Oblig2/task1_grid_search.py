2559086f1bd7e0ebadf29abeee2391ba72272ef5
Vetle Lode <vetlelode@gmail.com>
2020-10-17 02:22:32 +0200

    Final code changes

--- Oblig2/task1_grid_search.py
+++ Oblig2/task1_grid_search.py
@@ -10,9 +10,18 @@ X_tot = datasets.load_digits().data
 Y_tot = datasets.load_digits().target
 X, X_test, Y, Y_test = train_test_split(X_tot, Y_tot, test_size=0.2)
 
-parameters = {'hidden_layer_sizes': [x for x in itertools.product(np.arange(100, 600, 50), repeat=2)], 'solver': [
+parameters_3layers = {'hidden_layer_sizes': [x for x in itertools.product((50, 100, 200, 300, 400, 500), repeat=3)], 'solver': [
+    "lbfgs"], 'activation': ["tanh"], 'max_iter': [1000], 'verbose': [True]}
+parameters_2layers = {'hidden_layer_sizes': [x for x in itertools.product((50, 100, 200, 300, 400, 500), repeat=2)], 'solver': [
     "lbfgs"], 'activation': ["tanh"], 'max_iter': [1000], 'verbose': [True]}
 
-clf = GridSearchCV(MLPClassifier(), parameters, n_jobs=-1, iid=True).fit(X, Y)
-print(clf.score(X_test, Y_test))
-print(clf.best_params_)
+clf_3_layers = GridSearchCV(
+    MLPClassifier(), parameters_3layers, n_jobs=-1, iid=True).fit(X, Y)
+score3 = clf_3_layers.score(X_test, Y_test)
+
+clf_2_layers = GridSearchCV(
+    MLPClassifier(), parameters_2layers, n_jobs=-1, iid=True).fit(X, Y)
+score2 = clf_2_layers.score(X_test, Y_test)
+
+print("The highest score achived with 2 layers: {}, with params: {} \nThe highest score achived with 3 layers: {}, with params: {}".format(
+    round(score2, 3), clf_2_layers.best_params_, round(score3, 3), clf_3_layers.best_params_))
